b05aea65d88fb9442f722748f15e02b700864b45
RooDen <dokarasov@gmail.com>
2018-01-07 16:25:46 +0200

    Modifications from previous lessons

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies() => new List<Movie>
+        {
+            new Movie { Id = 1, Name = "Toradora!" },
+            new Movie() { Id = 2, Name = "Kimi no na wa." }
+        };
+    }
+}
